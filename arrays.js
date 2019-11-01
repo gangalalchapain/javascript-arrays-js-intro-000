6c57602d96755a4b541580e5bc62223956c4876f
Ganga Lal Chapain <gangalalchapain@gmail.com>
2019-11-01 21:29:35 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -10,9 +10,8 @@ function destructivelyAddElementToBeginningOfArray(array, element){
   return array;
 }
 
-/*
+
 function addElementToEndOfArray(array, element){
   array.push(element);
   return array;
 }
-*/
